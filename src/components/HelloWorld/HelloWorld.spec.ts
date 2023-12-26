d1101b4254d53ba540a7e78bddf58497ffd192ce
Mateus Sarmento <mateus.sarmento@qikserve.com>
2023-12-26 15:51:15 -0300

    Initial commit

---
+++ src/components/HelloWorld/HelloWorld.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+import { mount } from '@vue/test-utils';
+import HelloWorld from '../HelloWorld/HelloWorld.vue';
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    const wrapper = mount(HelloWorld, { props: { name: 'Hello Vitest' } });
+    expect(wrapper.text()).toContain('Hello Vitest');
+  });
+});
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from '@/components/HelloWorld/HelloWorld.vue';
+</script>
+
+<template>
+  <main>
+    <HelloWorld name="Vitest" />
+  </main>
+</template>
