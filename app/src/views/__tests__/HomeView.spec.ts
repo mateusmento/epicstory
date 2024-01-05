7a238cc8b19f28612c03d5a8596246ef6e00ed54
Mateus Sarmento <mateus.sarmento@qikserve.com>
2024-01-05 15:05:54 -0300

    Include msw server in home view spec

--- app/src/views/__tests__/HomeView.spec.ts
+++ app/src/views/__tests__/HomeView.spec.ts
@@ -1,7 +1,14 @@
 import { describe, it, expect } from 'vitest';
-
+import { setupServer } from 'msw/node';
+import { HttpResponse, http } from 'msw';
 import { mount } from '@vue/test-utils';
 import HomeView from '../HomeView.vue';
+
+const server = setupServer(
+  http.post('http://localhost:3000/auth/signup', async ({ request, params }) => {
+    return HttpResponse.json({ token: '' }, { status: 200 });
+  })
+);
 
 describe('HomeView', () => {
   it('renders properly', () => {
