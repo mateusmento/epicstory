4bdc3c05eca87bea58ac1baa1ae26268b8f575e7
Mateus Sarmento <omateusmento@gmail.com>
2024-07-20 09:00:44 -0300

    Update collapsible transitions

--- app/src/design-system/ui/collapsible/CollapsibleContent.vue
+++ app/src/design-system/ui/collapsible/CollapsibleContent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { CollapsibleContent, type CollapsibleContentProps } from "radix-vue";
 import { omit } from "lodash";
-type Props = CollapsibleContentProps & { transition?: "vertical" | "fade" | "none" };
+type Props = CollapsibleContentProps & { transition?: "vertical" | "horizontal" };
 
 const props = withDefaults(defineProps<Props>(), {
   transition: "vertical",
@@ -13,10 +13,10 @@ const props = withDefaults(defineProps<Props>(), {
     v-bind="omit(props, ['direction'])"
     class="overflow-hidden transition-all"
     :class="{
-      'data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down':
+      'data-[state=open]:animate-collapsible-vertically-show data-[state=closed]:animate-collapsible-vertically-hide':
         transition === 'vertical',
-      'data-[state=open]:animate-collapsible-fadein data-[state=closed]:animate-collapsible-fadeout':
-        transition === 'fade',
+      'data-[state=open]:animate-collapsible-horizontally-show data-[state=closed]:animate-collapsible-horizontally-hide':
+        transition === 'horizontal',
     }"
   >
     <slot />
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -86,13 +86,23 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
-        "collapsible-down": {
+        "collapsible-vertically-show": {
           from: { height: 0 },
           to: { height: "var(--radix-collapsible-content-height)" },
         },
-        "collapsible-up": {
+        "collapsible-vertically-hide": {
           from: { height: "var(--radix-collapsible-content-height)" },
           to: { height: 0 },
+        },
+        "collapsible-horizontally-show": {
+          from: { width: 0, transform: "scale(0.99)", opacity: 0 },
+          "50%": { transform: "scale(0.99)", opacity: 0 },
+          to: { width: "var(--radix-collapsible-content-width)", transform: "scale(1)", opacity: 1 },
+        },
+        "collapsible-horizontally-hide": {
+          from: { width: "var(--radix-collapsible-content-width)", transform: "scale(1)", opacity: 1 },
+          "50%": { transform: "scale(0.99)", opacity: 0 },
+          to: { width: 0, transform: "scale(0.99)", opacity: 0 },
         },
         "collapsible-fadeout": {
           from: { transform: "scale(1)", opacity: 1 },
@@ -106,10 +116,12 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "collapsible-down": "collapsible-down 0.2s ease-in-out",
-        "collapsible-up": "collapsible-up 0.2s ease-in-out",
-        "collapsible-fadein": "collapsible-fadein .1s ease-in-out",
-        "collapsible-fadeout": "collapsible-fadeout .1s ease-in-out",
+        "collapsible-vertically-show": "collapsible-vertically-show 0.2s ease-in-out",
+        "collapsible-vertically-hide": "collapsible-vertically-hide 0.2s ease-in-out",
+        "collapsible-horizontally-show": "collapsible-horizontally-show .2s ease-in-out",
+        "collapsible-horizontally-hide": "collapsible-horizontally-hide .2s ease-in-out",
+        "collapsible-fadein": "collapsible-fadein .2s ease-in-out",
+        "collapsible-fadeout": "collapsible-fadeout .2s ease-in-out",
       },
     },
   },
