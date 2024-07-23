d2f9c8af2e29aa7dd9d276715fc1aaf88ec64678
Mateus Sarmento <omateusmento@gmail.com>
2024-07-23 13:13:45 -0300

    Setup swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,22 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+
+  setupSwagger(app);
+
   await app.listen(3000);
+}
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Epicstory API')
+    .setDescription('Main API for epicstory application')
+    .setVersion('1.0')
+    .addTag('release')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 }
 
 bootstrap();
