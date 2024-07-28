95660a5dd38c0564e1ed197a97d1261626ed80da
Mateus Sarmento <omateusmento@gmail.com>
2024-07-28 17:00:44 -0300

    chore(api): Add channel module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { createTypeOrmModule } from './core/typeorm';
 import { WorkspaceModule } from './workspace/workspace.module';
+import { ChannelModule } from './channel/channel.module';
 
 @Module({
-  imports: [CoreModule, createTypeOrmModule(), AuthModule, WorkspaceModule],
+  imports: [
+    CoreModule,
+    createTypeOrmModule(),
+    AuthModule,
+    WorkspaceModule,
+    ChannelModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
