3ba8622319cf5a1fa894a55824d2838462549d59
Mateus Sarmento <omateusmento@gmail.com>
2024-08-23 16:01:03 -0300

    Update projects app pane

--- app/src/views/dashboard/projects/Projects.vue
+++ app/src/views/dashboard/projects/Projects.vue
@@ -21,35 +21,38 @@ watch(
 </script>
 
 <template>
-  <div class="flex:rows-xl p-4">
-    <Collapsible as-child>
-      <div class="flex:cols-auto flex:center-y">
-        <h1 class="flex:cols-md flex:center-y text-lg">
-          <Icon name="hi-clipboard-list" />
-          <div>Projects</div>
-        </h1>
+  <div class="flex:rows">
+    <div class="flex:rows-xl p-4">
+      <Collapsible as-child>
+        <div class="flex:cols-auto flex:center-y">
+          <h1 class="flex:cols-md flex:center-y text-lg">
+            <Icon name="hi-clipboard-list" />
+            <div>Projects</div>
+          </h1>
 
-        <CollapsibleTrigger as-child>
-          <Button variant="outline" size="badge" class="ml-auto">Create</Button>
-        </CollapsibleTrigger>
-      </div>
+          <CollapsibleTrigger as-child>
+            <Button variant="outline" size="badge" class="ml-auto">Create</Button>
+          </CollapsibleTrigger>
+        </div>
 
-      <CollapsibleContent>
-        <Form @submit="createProject($event as any)" class="flex:rows-lg">
-          <Field label="Name" name="name" placeholder="Create a project..." />
-          <Field type="hidden" name="workspaceId" :value="workspace.id" />
-          <Button size="xs">Create</Button>
-        </Form>
-      </CollapsibleContent>
-    </Collapsible>
+        <CollapsibleContent>
+          <Form @submit="createProject($event as any)" class="flex:rows-lg">
+            <Field label="Name" name="name" placeholder="Create a project..." />
+            <Field type="hidden" name="workspaceId" :value="workspace.id" />
+            <Button size="xs">Create</Button>
+          </Form>
+        </CollapsibleContent>
+      </Collapsible>
+    </div>
 
-    <div class="flex:rows-lg p-1 rounded-md bg-zinc-100 text-zinc-500 text-sm">
+    <div class="flex:rows">
       <div
         v-for="project of projects"
         :key="project.id"
-        class="px-2 py-1 rounded-sm hover:bg-zinc-200/60 cursor-pointer"
+        class="flex:rows-md p-4 border-t hover:bg-zinc-200/60 cursor-pointer"
       >
-        {{ project.name }}
+        <div class="text-base text-zinc-800 font-dmSans font-medium">{{ project.name }}</div>
+        <div class="text-xs text-zinc-500">4 members</div>
       </div>
     </div>
   </div>
