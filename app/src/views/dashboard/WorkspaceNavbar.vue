49c0fb032bbae307dc2cc9a91b8e48d267114fb9
Mateus Sarmento <omateusmento@gmail.com>
2024-08-23 10:34:49 -0300

    Update workspace navbar

--- app/src/views/dashboard/WorkspaceNavbar.vue
+++ app/src/views/dashboard/WorkspaceNavbar.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { UpdateUser, UserProfile } from "@/components/user";
+import { UpdateUser } from "@/components/user";
 import { Button, Dialog, DialogContent, DialogTrigger, NavTrigger } from "@/design-system";
-import { Icon, IconLeftCollapse } from "@/design-system/icons";
+import { Icon } from "@/design-system/icons";
+import { useAuth } from "@/domain/auth";
 import { useWorkspace } from "@/domain/workspace";
 import NavListItem from "./NavListItem.vue";
-import { useAuth } from "@/domain/auth";
 
 defineProps<{ isAppPaneOpen: boolean }>();
 
@@ -14,35 +14,47 @@ const { workspace } = useWorkspace();
 
 <template>
   <div class="flex:rows-xl h-full text-zinc-500">
-    <div class="flex:cols-md items-start p-2 w-full cursor-pointer">
-      <div class="flex:rows-sm mr-auto">
+    <div class="flex:rows-sm p-2 w-full mr-auto">
+      <div class="flex:cols flex:center-y">
         <div class="pl-1 text-xs text-zinc-500">Workspace</div>
-        <div class="flex:cols-lg flex:center-y">
-          <NavTrigger
-            view="app-pane"
-            content="workspaces"
-            :as="Button"
-            variant="ghost"
-            size="badge"
-            class="rounded-md text-base text-neutral-800 font-normal whitespace-nowrap text-ellipsis overflow-hidden hover:bg-zinc-200/60"
-          >
-            {{ workspace?.name }}
-          </NavTrigger>
-          <NavTrigger view="navbar" content="switch-workspace" :as="Button" variant="outline" size="badge">
-            Switch
-          </NavTrigger>
-        </div>
+
+        <div class="self:fill"></div>
+
+        <Dialog>
+          <DialogTrigger as-child>
+            <div class="w-6 h-6 mr-lg rounded-full cursor-pointer">
+              <img :src="user?.picture" />
+            </div>
+          </DialogTrigger>
+          <DialogContent>
+            <UpdateUser />
+          </DialogContent>
+        </Dialog>
+
+        <NavTrigger view="navbar" content="notifications" :as="Button" variant="ghost" size="icon">
+          <Icon name="io-notifications" />
+        </NavTrigger>
+
+        <NavTrigger view="navbar" content="settings" :as="Button" variant="ghost" size="icon">
+          <Icon name="md-settings-round" />
+        </NavTrigger>
       </div>
 
-      <NavTrigger view="navbar" content="notifications" :as="Button" variant="ghost" size="icon">
-        <Icon name="io-notifications" />
-      </NavTrigger>
-
-      <NavTrigger view="navbar" content="settings" :as="Button" variant="ghost" size="icon">
-        <Icon name="md-settings-round" />
-      </NavTrigger>
-
-      <!-- <IconLeftCollapse :class="{ 'scale-x-[-1]': !isAppPaneOpen }" /> -->
+      <div class="flex:cols-lg w-full flex:center-y">
+        <NavTrigger
+          view="app-pane"
+          content="workspaces"
+          :as="Button"
+          variant="ghost"
+          size="badge"
+          class="block rounded-md text-base text-neutral-800 font-normal whitespace-nowrap text-ellipsis overflow-hidden hover:bg-zinc-200/60"
+        >
+          {{ workspace?.name }}
+        </NavTrigger>
+        <NavTrigger view="navbar" content="switch-workspace" :as="Button" variant="outline" size="badge">
+          Switch
+        </NavTrigger>
+      </div>
     </div>
 
     <nav class="flex:rows-md font-semibold">
@@ -63,16 +75,5 @@ const { workspace } = useWorkspace();
         Members
       </NavListItem>
     </nav>
-
-    <div class="self:fill"></div>
-
-    <Dialog>
-      <DialogTrigger as-child>
-        <UserProfile />
-      </DialogTrigger>
-      <DialogContent>
-        <UpdateUser />
-      </DialogContent>
-    </Dialog>
   </div>
 </template>
