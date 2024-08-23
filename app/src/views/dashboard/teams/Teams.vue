2b3bcf9389fa9083687110bcc07cf46d61f04386
Mateus Sarmento <omateusmento@gmail.com>
2024-08-23 16:01:07 -0300

    Update teams app pane

--- app/src/views/dashboard/teams/Teams.vue
+++ app/src/views/dashboard/teams/Teams.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
-import { Button, Field, Form } from "@/design-system";
+import { Button, Collapsible, CollapsibleContent, CollapsibleTrigger, Field, Form } from "@/design-system";
+import { Icon } from "@/design-system/icons";
 import { useWorkspace } from "@/domain/workspace";
 import { onMounted } from "vue";
 
@@ -9,20 +10,38 @@ onMounted(() => fetchTeams());
 </script>
 
 <template>
-  <div class="flex:rows-xl p-4">
-    <h1>Teams</h1>
-    <div class="flex:rows-lg p-1 rounded-md bg-zinc-100 text-zinc-500 text-sm">
+  <div class="flex:rows">
+    <div class="flex:rows-xl p-4">
+      <Collapsible as-child>
+        <div class="flex:cols-auto flex:center-y">
+          <h1 class="flex:cols-md flex:center-y text-lg">
+            <Icon name="hi-clipboard-list" />
+            <div>Teams</div>
+          </h1>
+
+          <CollapsibleTrigger as-child>
+            <Button variant="outline" size="badge" class="ml-auto">Create</Button>
+          </CollapsibleTrigger>
+        </div>
+
+        <CollapsibleContent>
+          <Form @submit="createTeam($event.name)" class="flex:rows-lg">
+            <Field label="Name" name="name" placeholder="Create new team..." />
+            <Button type="submit" size="xs">Create</Button>
+          </Form>
+        </CollapsibleContent>
+      </Collapsible>
+    </div>
+
+    <div class="flex:rows">
       <div
         v-for="team in teams"
         :key="team.id"
-        class="px-2 py-1 rounded-sm hover:bg-zinc-200/60 cursor-pointer"
+        class="flex:rows-md p-4 border-t hover:bg-zinc-200/60 cursor-pointer"
       >
-        {{ team.name }}
+        <div class="text-base text-zinc-800 font-dmSans font-medium">{{ team.name }}</div>
+        <div class="text-xs text-zinc-500">4 members</div>
       </div>
     </div>
-    <Form @submit="createTeam($event.name)" class="flex:rows-lg">
-      <Field label="Name" name="name" placeholder="Create new team..." />
-      <Button type="submit" size="xs">Create</Button>
-    </Form>
   </div>
 </template>
