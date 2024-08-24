d0effa127c08073f2edf074da76caaddd5d92d02
Mateus Sarmento <omateusmento@gmail.com>
2024-08-24 11:22:48 -0300

    Update workspace navbar

--- app/src/components/user/UserProfile.vue
+++ app/src/components/user/UserProfile.vue
@@ -1,24 +1,23 @@
 <script lang="ts" setup>
-import { Button } from "@/design-system";
 import { Icon } from "@/design-system/icons";
-import { useAuthStore } from "@/domain/auth/user.store";
-import { storeToRefs } from "pinia";
+import { useAuth } from "@/domain/auth";
 
-const { user } = storeToRefs(useAuthStore());
+const { user } = useAuth();
 </script>
 
 <template>
-  <div class="flex:cols-sm p-4 rounded-md bg-zinc-200/60">
-    <div class="w-10 h-10 mr-lg rounded-full bg-zinc-300">
-      <img :src="user?.picture" />
+  <div class="flex:cols-sm flex:center-y p-xl rounded-md cursor-pointer hover:bg-zinc-200/60">
+    <img :src="user?.picture" class="w-10 h-10 mr-lg" />
+
+    <div class="flex:rows-sm flex-1 min-w-0">
+      <div class="text-base text-zinc-800 whitespace-nowrap text-ellipsis overflow-hidden">
+        {{ user?.name }}
+      </div>
+      <div class="text-xs text-zinc-500 whitespace-nowrap text-ellipsis overflow-hidden">
+        {{ user?.email }}
+      </div>
     </div>
-    <!-- <div class="text-base">{{ user?.name }}</div> -->
-    <!-- <div class="self:fill"></div> -->
-    <Button variant="ghost" size="icon">
-      <Icon name="io-notifications" />
-    </Button>
-    <Button variant="ghost" size="icon">
-      <Icon name="md-settings-round" />
-    </Button>
+
+    <Icon name="bi-chevron-expand" />
   </div>
 </template>
--- app/src/views/dashboard/WorkspaceNavbar.vue
+++ app/src/views/dashboard/WorkspaceNavbar.vue
@@ -1,58 +1,37 @@
 <script setup lang="ts">
-import { UpdateUser } from "@/components/user";
+import { UpdateUser, UserProfile } from "@/components/user";
 import { Button, Dialog, DialogContent, DialogTrigger, NavTrigger } from "@/design-system";
 import { Icon } from "@/design-system/icons";
-import { useAuth } from "@/domain/auth";
 import { useWorkspace } from "@/domain/workspace";
 import NavListItem from "./NavListItem.vue";
 
 defineProps<{ isAppPaneOpen: boolean }>();
 
-const { user } = useAuth();
 const { workspace } = useWorkspace();
 </script>
 
 <template>
   <div class="flex:rows-xl h-full text-zinc-500">
-    <div class="flex:rows-sm p-2 w-full mr-auto">
-      <div class="flex:cols flex:center-y">
-        <div class="pl-1 text-xs text-zinc-500">Workspace</div>
+    <div class="flex:rows-md p-2 w-full mr-auto">
+      <div class="pl-1 text-xs text-zinc-500">Workspace</div>
 
-        <div class="self:fill"></div>
-
-        <Dialog>
-          <DialogTrigger as-child>
-            <div class="w-6 h-6 mr-lg rounded-full cursor-pointer">
-              <img :src="user?.picture" />
-            </div>
-          </DialogTrigger>
-          <DialogContent>
-            <UpdateUser />
-          </DialogContent>
-        </Dialog>
-
-        <NavTrigger view="navbar" content="notifications" :as="Button" variant="ghost" size="icon">
-          <Icon name="io-notifications" />
-        </NavTrigger>
-
-        <NavTrigger view="navbar" content="settings" :as="Button" variant="ghost" size="icon">
-          <Icon name="md-settings-round" />
-        </NavTrigger>
-      </div>
-
-      <div class="flex:cols-lg w-full flex:center-y">
+      <div class="flex:cols-md flex:center-y w-full">
         <NavTrigger
-          view="app-pane"
-          content="workspaces"
+          view="navbar"
+          content="switch-workspace"
           :as="Button"
           variant="ghost"
           size="badge"
           class="block rounded-md text-base text-neutral-800 font-normal whitespace-nowrap text-ellipsis overflow-hidden hover:bg-zinc-200/60"
         >
           {{ workspace?.name }}
+          <Icon name="oi-chevron-down" />
         </NavTrigger>
-        <NavTrigger view="navbar" content="switch-workspace" :as="Button" variant="outline" size="badge">
-          Switch
+
+        <div class="self:fill"></div>
+
+        <NavTrigger view="navbar" content="settings" :as="Button" variant="ghost" size="icon">
+          <Icon name="md-settings-round" />
         </NavTrigger>
       </div>
     </div>
@@ -62,13 +41,18 @@ const { workspace } = useWorkspace();
         <Icon name="oi-inbox" />
         Inbox
       </NavListItem>
-      <NavListItem view="app-pane" content="projects" class="flex:cols-md flex:center-y">
-        <Icon name="hi-clipboard-list" />
-        Projects
+      <NavListItem view="app-pane" content="inbox" class="flex:cols-md flex:center-y">
+        <Icon name="oi-apps" />
+        Issues
       </NavListItem>
       <NavListItem view="app-pane" content="channels" class="flex:cols-md flex:center-y">
         <Icon name="fa-slack-hash" />
         Channels
+        <Icon name="bi-chevron-expand" class="ml-auto" />
+      </NavListItem>
+      <NavListItem view="app-pane" content="projects" class="flex:cols-md flex:center-y">
+        <Icon name="hi-clipboard-list" />
+        Projects
       </NavListItem>
       <NavListItem view="app-pane" content="teams" class="flex:cols-md flex:center-y">
         <Icon name="bi-person-workspace" />
@@ -79,5 +63,16 @@ const { workspace } = useWorkspace();
         Members
       </NavListItem>
     </nav>
+
+    <div class="self:fill"></div>
+
+    <Dialog>
+      <DialogTrigger as-child>
+        <UserProfile />
+      </DialogTrigger>
+      <DialogContent>
+        <UpdateUser />
+      </DialogContent>
+    </Dialog>
   </div>
 </template>
