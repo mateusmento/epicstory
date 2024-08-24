d9edd1e80e30903dbf0cf1e7148607c25cce0759
Mateus Sarmento <omateusmento@gmail.com>
2024-08-24 11:21:07 -0300

    Add icons in message writer

--- app/src/design-system/channel/MessageWriter.vue
+++ app/src/design-system/channel/MessageWriter.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
 import { ref } from "vue";
+import { Icon } from "../icons";
 import { Button } from "../ui/button";
-import IconSendMessage from "../icons/IconSendMessage.vue";
+import { Separator } from "../ui/separator";
 
 const emit = defineEmits<{
   (e: "send-message", value: { content: string }): void;
@@ -18,23 +19,36 @@ function onSendMessage() {
 
 <template>
   <div
-    class="p-2 border border-zinc-200 rounded-xl focus-within:outline outline-1 outline-zinc-300/60"
+    class="p-3 border border-zinc-200 rounded-xl focus-within:outline outline-1 outline-zinc-300/60"
     @click="messageTextEl?.focus()"
   >
     <textarea
       v-model="messageContent"
+      placeholder="Send a message to channel..."
       class="w-full h-full px-2 rounded-md resize-none outline-none"
       ref="messageTextEl"
     />
-    <div class="">
+
+    <div class="flex:cols-xl flex:center-y text-zinc-500">
+      <Icon name="bi-camera-video" class="w-6 h-6" />
+      <Icon name="bi-mic" class="w-6 h-6" />
+      <Separator orientation="vertical" class="h-8 bg-zinc-300" />
+      <Icon name="co-smile" class="w-6 h-6" />
+      <Icon name="oi-mention" class="w-6 h-6" />
+      <Icon name="bi-image" class="w-6 h-6" />
+      <Icon name="ri-attachment-2" class="w-6 h-6" />
+
+      <div class="self:fill"></div>
+
       <Button
         legacy
         legacy-variant="primary"
         legacy-size="sm"
-        class="flex:cols-lg flex:center-y text-sm ml-auto bg-[#3A66FF]"
+        class="flex:cols-lg flex:center-y text-sm bg-[#3A66FF]"
         @click="onSendMessage"
       >
-        <IconSendMessage />
+        <Icon name="io-paper-plane" />
+        <!-- <Icon name="bi-send-fill" /> -->
         Send
       </Button>
     </div>
