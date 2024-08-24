aae35950132205ad5b209073fe0baac17e1a35a0
Mateus Sarmento <omateusmento@gmail.com>
2024-08-24 11:20:43 -0300

    Highlight nav list item when active

--- app/src/views/dashboard/NavListItem.vue
+++ app/src/views/dashboard/NavListItem.vue
@@ -1,14 +1,24 @@
 <script lang="ts" setup>
 import { NavTrigger } from "@/design-system";
+import { useNavTrigger } from "@/design-system/ui/nav-view/nav-view";
+import { computed } from "vue";
 
 const props = defineProps<{
   view: string;
   content: string;
 }>();
+
+const { viewContent } = useNavTrigger(props.view);
+
+const active = computed(() => props.content === viewContent.value);
 </script>
 
 <template>
-  <NavTrigger v-bind="props" class="px-2 py-1.5 rounded-md text-zinc-500 hover:bg-zinc-200/60 cursor-pointer">
+  <NavTrigger
+    v-bind="props"
+    class="px-2 py-1.5 rounded-md text-zinc-500 hover:bg-zinc-200/60 cursor-pointer"
+    :class="{ 'bg-zinc-200/60': active }"
+  >
     <slot />
   </NavTrigger>
 </template>
