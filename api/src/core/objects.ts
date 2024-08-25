76c93cdf9ad75d24ec2e47720f65ed03b955c5aa
Mateus Sarmento <omateusmento@gmail.com>
2024-08-25 11:04:46 -0300

    Remove functions.ts and replace with objects.ts

--- api/src/core/objects.ts
+++ api/src/core/objects.ts
@@ -1,12 +1,18 @@
+import { ClassTransformOptions } from '@nestjs/common/interfaces/external/class-transform-options.interface';
 import {
   ClassConstructor,
   plainToClass,
   plainToClassFromExist,
 } from 'class-transformer';
 
-export function create<T>(ctor: ClassConstructor<T>, partial: Partial<T>) {
+export function create<T>(
+  ctor: ClassConstructor<T>,
+  partial: Partial<T>,
+  options: ClassTransformOptions = {},
+) {
   return plainToClass(ctor, partial, {
     enableCircularCheck: true,
+    ...options,
   });
 }
 
--- api/src/workspace/application/reactions/user-created.reaction.ts
+++ api/src/workspace/application/reactions/user-created.reaction.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { OnEvent } from '@nestjs/event-emitter';
 import { UserCreatedEvent } from 'src/auth/contracts/user-created.event';
-import { create } from 'src/core/functions';
+import { create } from 'src/core/objects';
 import { CreateProject } from '../features/project/create-project.command';
 import { CreateWorkspace } from '../features/workspace/create-workspace.command';
 
@@ -13,10 +13,14 @@ export class UserCreatedReaction {
   @OnEvent(UserCreatedEvent)
   async react(user: any) {
     const workspace = await this.commandBus.execute(
-      create(CreateWorkspace, {
-        name: `${user.name}'s Workspace`,
-        issuerId: user.id,
-      }),
+      create(
+        CreateWorkspace,
+        {
+          name: `${user.name}'s Workspace`,
+          issuerId: user.id,
+        },
+        { enableImplicitConversion: true },
+      ),
     );
 
     await this.commandBus.execute(
