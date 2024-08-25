c732a3a484fdb576ad61f82603da236e81a90b92
Mateus Sarmento <omateusmento@gmail.com>
2024-08-25 09:39:25 -0300

    Implement find issues

--- api/src/workspace/application/features/project/find-issues.query.ts
+++ api/src/workspace/application/features/project/find-issues.query.ts
@@ -1,8 +1,19 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { IsNumber, IsOptional } from 'class-validator';
 import { patch } from 'src/core/objects';
+import { IssueRepository } from 'src/workspace/infrastructure/repositories/issue.repository';
 
 export class FindIssues {
-  productId: number;
+  @IsNumber()
+  workspaceId: number;
+
+  @IsNumber()
+  @IsOptional()
+  projectId?: number;
+
+  @IsNumber()
+  @IsOptional()
+  assigneeId?: number;
 
   constructor(data: Partial<FindIssues> = {}) {
     patch(this, data);
@@ -11,5 +22,13 @@ export class FindIssues {
 
 @QueryHandler(FindIssues)
 export class FindIssuesQuery implements IQueryHandler<FindIssues> {
-  async execute({}: FindIssues) {}
+  constructor(private issueRepo: IssueRepository) {}
+
+  async execute({ workspaceId, projectId }: FindIssues) {
+    const content = await this.issueRepo.find({
+      where: { workspaceId, projectId },
+    });
+
+    return { content };
+  }
 }
