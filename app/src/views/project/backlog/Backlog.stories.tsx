cc1cbf9a9b1198c3aa01ca7f70bc84b883dd9bc2
Mateus Sarmento <omateusmento@gmail.com>
2024-08-28 14:01:46 -0300

    Add DataList usage in story

---
+++ app/src/views/project/backlog/Backlog.stories.tsx
@@ -0,0 +1,60 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/design-system/ui/select";
+import type { Meta, StoryObj } from "@storybook/vue3";
+import DataList from "./DataList.vue";
+import type { ColumnDef } from "@tanstack/vue-table";
+import { Icon } from "@/design-system/icons";
+
+const meta = {
+  title: "Application/Views/Backlog",
+} satisfies Meta;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => ({
+    setup() {
+      const columns: ColumnDef<{ id: number; name: string }>[] = [
+        {
+          accessorKey: "id",
+        },
+        {
+          accessorKey: "name",
+        },
+        {
+          id: "remove",
+          header: "Actions",
+          cell: () => (
+            <div>
+              <Icon name="io-trash-bin" class="cursor-pointer text-zinc-800" />
+            </div>
+          ),
+        },
+      ];
+      const data = [
+        {
+          id: 1,
+          name: "Mateus Sarmento",
+        },
+        {
+          id: 2,
+          name: "Mateus Sarmento",
+        },
+      ];
+      return () => (
+        <div class="flex:rows-md w-96">
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select a fruit" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="option 1">Option 1</SelectItem>
+            </SelectContent>
+          </Select>
+          <DataList columns={columns} data={data} />
+        </div>
+      );
+    },
+  }),
+};
