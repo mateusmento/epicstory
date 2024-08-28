9c5b79e46aff0152e0e769f33bd36839ccbddd2c
Mateus Sarmento <omateusmento@gmail.com>
2024-08-28 14:00:48 -0300

    Improve issue list into table layout with sortable function

--- app/src/views/project/backlog/Backlog.vue
+++ app/src/views/project/backlog/Backlog.vue
@@ -16,11 +16,16 @@ const props = defineProps<{ projectId: string }>();
 const { issues, fetchIssues, createIssue, updateIssue, removeIssue, addAssignee } = useIssues();
 
 const orderBy = ref("createdAt");
+const order = ref("desc");
+
+function toggleOrder() {
+  order.value = order.value === "asc" ? "desc" : "asc";
+}
 
 onMounted(() => {
   fetchIssues({
     projectId: +props.projectId,
-    order: "desc",
+    order: order.value,
     orderBy: orderBy.value,
     page: 0,
     count: 50,
@@ -28,11 +33,11 @@ onMounted(() => {
 });
 
 watch(
-  () => [props.projectId, orderBy.value],
+  () => [props.projectId, orderBy.value, order.value],
   () => {
     fetchIssues({
       projectId: +props.projectId,
-      order: "desc",
+      order: order.value,
       orderBy: orderBy.value,
       page: 0,
       count: 50,
@@ -92,67 +97,86 @@ function issueStatusColor(status: string) {
       </Select>
     </div>
 
-    <ScrollArea class="flex-1 min-h-0 pr-4">
-      <div class="flex:rows-md">
-        <div v-for="issue of issues" :key="issue.id" class="flex:cols-md flex:center-y">
-          <Button
-            variant="outline"
-            size="badge"
-            @click="updateIssueStatus(issue)"
-            :class="cn(issueStatusColor(issue.status))"
-            >{{ issue.status }}</Button
+    <div class="grid grid-cols-[auto_1fr_auto_auto_auto_auto_auto] gap-y-md flex-1 min-h-0 pr-4">
+      <div class="grid grid-cols-subgrid col-span-7 gap-x-lg">
+        <div>Status</div>
+        <div>Title</div>
+        <div
+          @click="
+            orderBy = 'priority';
+            toggleOrder();
+          "
+        >
+          Priority
+        </div>
+        <div class="col-span-2">Assignees</div>
+        <div class="col-span-2">Due date</div>
+      </div>
+      <div class="grid grid-cols-subgrid col-span-7 overflow-y-auto">
+        <div class="grid grid-cols-subgrid col-span-7 gap-y-md">
+          <div
+            v-for="issue of issues"
+            :key="issue.id"
+            class="grid grid-cols-subgrid col-span-7 gap-x-lg items-center"
           >
-          <div v-if="edittingIssue.id !== issue.id" @dblclick="openIssueEdit(issue)" class="text-sm">
-            {{ issue.title }}
-          </div>
-          <Form v-else @submit="saveEdit" class="flex:cols-md flex:center-y">
-            <Field v-model="edittingIssue.title" size="badge" name="title" />
-            <Button type="submit" size="badge">Save</Button>
-            <Button type="button" size="badge" @click="closeIssueEdit()">Cancel</Button>
-          </Form>
-          <div class="self:fill"></div>
-          <div>
-            <PriorityToggler
-              :value="issue.priority"
-              @update:value="updateIssue(issue.id, { priority: $event })"
+            <Button
+              variant="outline"
+              size="badge"
+              @click="updateIssueStatus(issue)"
+              :class="cn(issueStatusColor(issue.status))"
+              >{{ issue.status }}</Button
+            >
+            <div v-if="edittingIssue.id !== issue.id" @dblclick="openIssueEdit(issue)" class="text-sm">
+              {{ issue.title }}
+            </div>
+            <Form v-else @submit="saveEdit" class="flex:cols-md flex:center-y">
+              <Field v-model="edittingIssue.title" size="badge" name="title" />
+              <Button type="submit" size="badge">Save</Button>
+              <Button type="button" size="badge" @click="closeIssueEdit()">Cancel</Button>
+            </Form>
+            <div>
+              <PriorityToggler
+                :value="issue.priority"
+                @update:value="updateIssue(issue.id, { priority: $event })"
+              />
+            </div>
+            <div class="flex:cols">
+              <img
+                v-for="(assignee, i) of issue.assignees"
+                :key="assignee.id"
+                :src="assignee.picture"
+                :class="cn('w-5 h-5', i > 0 && 'ml-[-0.5rem]')"
+              />
+            </div>
+            <Combobox
+              v-model="selectedUser"
+              v-model:searchTerm="query"
+              :options="users"
+              track-by="id"
+              label-by="name"
+              @update:model-value="
+                selectedUser && addAssignee(issue.id, selectedUser.id);
+                selectedUser = undefined;
+              "
+            >
+              <template #trigger>
+                <div class="rounded-full border-2 border-dashed flex flex:center p-0.5 cursor-pointer">
+                  <Icon name="fa-user-plus" class="w-4 h-4 text-zinc-400" />
+                </div>
+              </template>
+            </Combobox>
+            <DueDatePicker
+              size="badge"
+              :modelValue="issue.dueDate ? parseAbsolute(issue.dueDate, 'America/Sao_Paulo') : undefined"
+              @update:model-value="
+                updateIssue(issue.id, { dueDate: $event.toDate('America/Sao_Paulo').toString() })
+              "
             />
+            <Icon name="io-trash-bin" @click="removeIssue(issue.id)" class="cursor-pointer text-zinc-800" />
           </div>
-          <div class="flex:cols">
-            <img
-              v-for="(assignee, i) of issue.assignees"
-              :key="assignee.id"
-              :src="assignee.picture"
-              :class="cn('w-5 h-5', i > 0 && 'ml-[-0.5rem]')"
-            />
-          </div>
-          <Combobox
-            v-model="selectedUser"
-            v-model:searchTerm="query"
-            :options="users"
-            track-by="id"
-            label-by="name"
-            @update:model-value="
-              selectedUser && addAssignee(issue.id, selectedUser.id);
-              selectedUser = undefined;
-            "
-          >
-            <template #trigger>
-              <div class="rounded-full border border-2 border-dashed flex flex:center p-0.5 cursor-pointer">
-                <Icon name="fa-user-plus" class="w-4 h-4 text-zinc-400" />
-              </div>
-            </template>
-          </Combobox>
-          <DueDatePicker
-            size="badge"
-            :modelValue="issue.dueDate ? parseAbsolute(issue.dueDate, 'America/Sao_Paulo') : undefined"
-            @update:model-value="
-              updateIssue(issue.id, { dueDate: $event.toDate('America/Sao_Paulo').toString() })
-            "
-          />
-          <Icon name="io-trash-bin" @click="removeIssue(issue.id)" class="cursor-pointer text-zinc-800" />
         </div>
       </div>
-    </ScrollArea>
+    </div>
 
     <Form @submit="createIssue(+projectId, $event.title)" class="flex:cols-md">
       <Field name="title" size="xs" placeholder="Describe an issue..." class="flex-1" />
