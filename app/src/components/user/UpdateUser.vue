fa9eed85b725b0f553ccac02c201f3891a0cb24d
Mateus Sarmento <omateusmento@gmail.com>
2024-09-09 07:59:33 -0300

    Include drop zone in user picture upload

--- app/src/components/user/UpdateUser.vue
+++ app/src/components/user/UpdateUser.vue
@@ -22,8 +22,18 @@ function setPicture(file: File) {
   reader.readAsDataURL(file);
 }
 
+const newPictureIsOver = ref(false);
+
 useDropZone(fileDrop, {
+  onEnter() {
+    newPictureIsOver.value = true;
+  },
+  onLeave() {
+    newPictureIsOver.value = false;
+  },
   onDrop(files) {
+    console.log(files);
+    newPictureIsOver.value = false;
     setPicture((files ?? [])[0]);
   },
 });
@@ -39,10 +49,27 @@ function onSave() {
 <template>
   <div class="flex:rows-lg">
     <label ref="fileDrop" class="self-center">
-      <img :src="pictureUrl" class="rounded-full" />
+      <img v-if="!newPictureIsOver" :src="pictureUrl" class="rounded-full" />
+      <div v-else class="dropzone w-96 h-96 flex items-center justify-center">Drop a new picture</div>
       <input type="file" hidden @change="setPicture((($event.target as any).files ?? [])[0])" />
     </label>
     <Button size="xs" @click="onSave">Save</Button>
     <Button @click="signOut">Sign out</Button>
   </div>
 </template>
+
+<style scoped>
+.dropzone {
+  border: dashed 4px #ccc; /*optional*/
+
+  border-image-source: url("https://i.sstatic.net/LKclP.png");
+  border-image-slice: 2;
+  border-image-repeat: round;
+
+  /* or use the shorthand border-image */
+  border-image: url("https://i.sstatic.net/LKclP.png") 2 round;
+
+  border-image-source: url("https://i.sstatic.net/LKclP.png");
+  margin: 0 20px;
+}
+</style>
