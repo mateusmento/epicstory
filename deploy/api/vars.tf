375aaa3b272c39aeae8fe6eeb4a185258c1143d8
Mateus <omateusmento@gmail.com>
2024-12-27 09:40:06 -0300

    Copy deployments files from previous repo

---
+++ deploy/api/vars.tf
@@ -0,0 +1,43 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "AWS_REGISTRY" {
+  type = string
+}
+
+variable "SERVICE_NAME" {
+  type = string
+}
+
+variable "SERVICE_VERSION" {
+  type = string
+}
+
+variable "LB_NAME_TAG" {
+  type = string
+}
+
+variable "GOOGLE_CLIENT_ID" {
+  type = string
+}
+
+variable "GOOGLE_CLIENT_SECRET" {
+  type = string
+}
+
+variable "GOOGLE_CALLBACK_URI" {
+  type = string
+}
+
+variable "GOOGLE_APP_REDIRECT_URL" {
+  type = string
+}
