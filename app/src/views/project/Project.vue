8137382a2563b40c5ad168202fe5bed90a77eafd
Mateus <omateusmento@gmail.com>
2025-01-01 15:09:58 -0300

    feat: add active tab highlight in project page

--- app/src/views/project/Project.vue
+++ app/src/views/project/Project.vue
@@ -1,18 +1,28 @@
 <script lang="ts" setup>
 import { Button, Separator } from "@/design-system";
-import { RouterLink } from "vue-router";
+import { RouterLink, useRoute } from "vue-router";
 
 defineProps<{ projectId: string }>();
+
+const currentRoute = useRoute();
+
+function isActive(route: string) {
+  return currentRoute.path.endsWith("/" + route);
+}
 </script>
 
 <template>
   <div class="h-full flex:rows">
     <nav class="flex:cols-lg flex:center-y p-2">
       <RouterLink :to="`/project/${projectId}/backlog`">
-        <Button variant="ghost" size="xs">Backlog</Button>
+        <Button variant="ghost" size="xs" :class="{ 'bg-zinc-200/50 text-zinc-500': isActive('backlog') }"
+          >Backlog</Button
+        >
       </RouterLink>
       <RouterLink :to="`/project/${projectId}/board`">
-        <Button variant="ghost" size="xs">Board</Button>
+        <Button variant="ghost" size="xs" :class="{ 'bg-zinc-200/50 text-zinc-500': isActive('board') }"
+          >Board</Button
+        >
       </RouterLink>
     </nav>
     <Separator />
