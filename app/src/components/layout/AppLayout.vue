8880a280e87a269171f914a864fa3bc08476cbfa
Mateus <omateusmento@gmail.com>
2025-01-07 12:45:12 -0300

    improve drawer pane control in app layout

--- app/src/components/layout/AppLayout.vue
+++ app/src/components/layout/AppLayout.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { Navbar } from "./navbar";
 import { DrawerPane } from "./drawer-pane";
 
-const isAppPaneOpen = ref(false);
+const openPane = ref<string>();
+
+const isAppPaneOpen = computed(() => openPane.value === "app-pane");
 const appPaneContent = ref("");
 
-const isDetailsPaneOpen = ref(false);
+const isDetailsPaneOpen = computed(() => openPane.value === "details-pane");
 const detailsPaneContent = ref("");
 </script>
 
@@ -20,16 +22,26 @@ const detailsPaneContent = ref("");
       <main
         class="flex:cols self:fill bg-white mt-2 rounded-tl-lg border border-zinc-300/60 shadow-md shadow-zinc-300/60"
       >
-        <DrawerPane view="app-pane" v-model:open="isAppPaneOpen" v-model:content="appPaneContent">
+        <DrawerPane
+          view="app-pane"
+          :open="isAppPaneOpen"
+          @update:open="openPane = $event ? 'app-pane' : undefined"
+          v-model:content="appPaneContent"
+        >
           <slot name="app-pane" />
         </DrawerPane>
 
         <!-- Main Content -->
-        <section class="self:fill">
+        <section class="self:fill min-w-0">
           <slot name="main-content" />
         </section>
 
-        <DrawerPane view="details-pane" v-model:open="isDetailsPaneOpen" v-model:content="detailsPaneContent">
+        <DrawerPane
+          view="details-pane"
+          :open="isDetailsPaneOpen"
+          @update:open="openPane = $event ? 'details-pane' : undefined"
+          v-model:content="detailsPaneContent"
+        >
           <slot name="details-pane" />
         </DrawerPane>
       </main>
